508b9fe4b13f9c732264c8f3ecf2241a80678147
Patryk Vincerre <bazior.patryk@gmail.com>
2023-08-01 12:39:51 +0200

    add min max in ads model

--- models/ad.model.js
+++ models/ad.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const adSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  text: { type: String, required: true },
+  title: { type: String, required: true, minLength: 10, maxLength: 50 },
+  text: { type: String, required: true, minLength: 20, maxLength: 1000 },
   date: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
