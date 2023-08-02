d863d206e3b741a1a7efbb027b04d8a79eb92632
Patryk Vincerre <bazior.patryk@gmail.com>
2023-08-02 19:50:45 +0200

    build

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
     secret: `${process.env.SESSION_SECRET}`,
-    store: MongoStore.create(mongoose.connection),
+    store: MongoStore.create(db),
     resave: false,
     saveUninitialized: false,
     cookie: {
